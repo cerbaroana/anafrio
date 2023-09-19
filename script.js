18e4469510be6e387dce0668020524bed619efe9
cerbaroana <137786775+cerbaroana@users.noreply.github.com>
2023-09-19 10:14:57 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+botao.addEventListener("click", function () {
+  var nomeCliente = nome.value;
+  var quantiaSorvete = quantidade.value;
+  var saborSorvete = sabores.value;
+
+  //alert("nome:" + nomeCliente + "Qnt:" + quantiaSorvete + "sabor: " + saborSorvete);
+
+  pedidoNome.innerHTML = nomeCliente;
+  pedidoQuantia.innerHTML = quantiaSorvete;
+  pedidoSabor.innerHTML = saborSorvete;
+});
